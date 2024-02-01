0ab84bf8643a48ff57c47f2d0e0c98f8d0ee15cd
dasdeo <mark@anio.software>
2024-02-01 17:45:25 +0100

    Use @anio-js-foundation/bundle-package.

--- example.mjs
+++ example.mjs
@@ -1,4 +1,4 @@
-import createTemporaryResource from "./index.mjs"
+import createTemporaryResource from "./src/index.mjs"
 
 const {location, cleanup} = await createTemporaryResource(
 	`console.log("hello")`, "text/javascript"

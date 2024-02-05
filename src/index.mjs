70b8900d3330dd70de7eefae6fba010667b12255
dasdeo <mark@anio.software>
2024-02-05 22:45:04 +0100

    Fix dynamic imports (use default import).

--- src/index.mjs
+++ src/index.mjs
@@ -2,10 +2,10 @@ import isNode from "@anio-js-core-foundation/is-node"
 
 export default async function createTemporaryResource(data, type = "text/plain") {
 	if (isNode()) {
-		const fs = await import("node:fs")
-		const os = await import("node:os")
-		const path = await import("node:path")
-		const crypto = await import("node:crypto")
+		const {default: fs} = await import("node:fs")
+		const {default: os} = await import("node:os")
+		const {default: path} = await import("node:path")
+		const {default: crypto} = await import("node:crypto")
 
 		const tmpname = crypto.randomBytes(16).toString("hex")
 		let extension = ".txt"

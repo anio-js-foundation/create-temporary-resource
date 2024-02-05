594c5cd37c40bdf55111cf7fc4c4b531fb5ec879
dasdeo <mark@anio.software>
2024-02-05 23:19:05 +0100

    Convert second parameter into an options object that has two keys: type and auto_cleanup for node contexts.

--- src/index.mjs
+++ src/index.mjs
@@ -1,11 +1,43 @@
 import isNode from "@anio-js-core-foundation/is-node"
 
-export default async function createTemporaryResource(data, type = "text/plain") {
+let node_global_vars = {
+	cleanupHandler_set: false,
+	cleanup_items: [],
+	node_modules: {
+		fs: null
+	}
+}
+
+function node_cleanupHandler() {
+	const {fs} = node_global_vars.node_modules
+
+	while (node_global_vars.cleanup_items.length) {
+		const item = node_global_vars.cleanup_items.shift()
+
+		try {
+			fs.unlinkSync(item)
+		} catch {}
+	}
+}
+
+export default async function createTemporaryResource(
+	data, {type = "text/plain", auto_cleanup = true} = {}
+) {
 	if (isNode()) {
 		const {default: fs} = await import("node:fs")
 		const {default: os} = await import("node:os")
 		const {default: path} = await import("node:path")
 		const {default: crypto} = await import("node:crypto")
+		const {default: process} = await import("node:process")
+
+		if (!node_global_vars.cleanupHandler_set) {
+			node_global_vars.node_modules.fs = fs
+
+			process.on("SIGTERM", node_cleanupHandler)
+			process.on("exit", node_cleanupHandler)
+
+			node_global_vars.cleanupHandler_set = true
+		}
 
 		const tmpname = crypto.randomBytes(16).toString("hex")
 		let extension = ".txt"
@@ -18,12 +50,18 @@ export default async function createTemporaryResource(data, type = "text/plain")
 
 		fs.writeFileSync(tmppath, data)
 
+		const location = fs.realpathSync(tmppath)
+
+		if (auto_cleanup) {
+			node_global_vars.cleanup_items.push(location)
+		}
+
 		return {
-			location: fs.realpathSync(tmppath),
+			location,
 			cleanup() {
 				try {
-					fs.unlinkSync(tmppath)
-				} catch { }
+					fs.unlinkSync(location)
+				} catch {}
 			}
 		}
 	} else {
@@ -38,4 +76,3 @@ export default async function createTemporaryResource(data, type = "text/plain")
 		}
 	}
 }
-
